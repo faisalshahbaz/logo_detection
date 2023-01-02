1c347cf3c6bb23ef7d36ab520203004224d9d77f
faisalshahbaz <faisalshahbaz@folio3.com>
2023-01-02 15:55:22 +0500

    file update

--- yolomodel.py
+++ yolomodel.py
@@ -1,6 +1,5 @@
 import torch
 import os
 
-modelpath = os.getcwd()
-model = torch.hub.load('ultralytics/yolov5', 'custom', path=modelpath+'/pepsi-model.pt', device="cpu")
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='pepsi-model.pt', device="cpu")
 
